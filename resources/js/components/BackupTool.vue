f0db53e0a27a530a9b0c7a6663cc10bb1348b0ed
Angelo Calabrò <angelocala94@gmail.com>
2019-09-24 16:04:03 +0200

    Added Package Settings
    - Added “polling” enable/disable setting
    - Added “polling_interval” setting

--- resources/js/components/BackupTool.vue
+++ resources/js/components/BackupTool.vue
@@ -129,14 +129,16 @@ export default {
         },
 
         startPolling() {
-            const poller = window.setInterval(() => {
-                this.updateBackupStatuses();
-                this.updateActiveDiskBackups();
-            }, 1000);
+            if(Nova.config.novabackuptool.polling){
+                const poller = window.setInterval(() => {
+                    this.updateBackupStatuses();
+                    this.updateActiveDiskBackups();
+                }, Nova.config.novabackuptool.polling_interval * 1000);
 
-            this.$once('hook:beforeDestroy', () => {
-                window.clearInterval(poller);
-            });
+                this.$once('hook:beforeDestroy', () => {
+                    window.clearInterval(poller);
+                });
+            }
         },
     },
 };
--- src/BackupToolServiceProvider.php
+++ src/BackupToolServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Spatie\BackupTool;
 
 use Laravel\Nova\Nova;
+use Laravel\Nova\Events\ServingNova;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\ServiceProvider;
 use Spatie\BackupTool\Http\Middleware\Authorize;
@@ -11,6 +12,10 @@ class BackupToolServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $this->publishes([
+            __DIR__.'/../config/novabackuptool.php' => config_path('novabackuptool.php'),
+        ], 'config');
+
         $this->publishes([
             __DIR__.'/../resources/lang/' => resource_path('lang/vendor/nova-backup-tool'),
         ]);
@@ -22,6 +27,13 @@ class BackupToolServiceProvider extends ServiceProvider
         $this->app->booted(function () {
             $this->routes();
         });
+
+        $this->provideConfigToScript();
+    }
+
+    public function register()
+    {
+        $this->mergeConfigFrom(__DIR__.'/../config/novabackuptool.php', 'novabackuptool');
     }
 
     protected function routes()
@@ -35,6 +47,18 @@ class BackupToolServiceProvider extends ServiceProvider
             ->group(
                 __DIR__.'/../routes/api.php'
             );
+    }
+
+    protected function provideConfigToScript()
+    {
+        Nova::serving(function (ServingNova $event) {
+            Nova::provideToScript([
+                'novabackuptool' => [
+                    'polling' => config('novabackuptool.polling'),
+                    'polling_interval' => config('novabackuptool.polling_interval')
+                ]
+            ]);
+        });
     }
 
     protected function registerTranslations()
